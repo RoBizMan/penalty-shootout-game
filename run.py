313cd427f9651fa9038eccfa0024f91b17cb8cb9
Robert T <robert@tiffert.eu>
2024-04-13 19:02:32 +0000

    Add request input player name to begin the game

--- run.py
+++ run.py
@@ -2,8 +2,25 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
+
+def request_player_name():
+    """Function to get the player's name and handle errors
+    if the input contains other than A-Z alphabets or/and
+    exceeds 40 characters"""
+    while True:
+        player_name = input(Fore.YELLOW + "Enter your name to start the game: ")
+        if not player_name.isalpha():
+            print(Fore.RED + "Error: Name should only contain alphabets A-Z or a-z.")
+        elif len(player_name) > 40:
+            print(Fore.RED + "Error: Name should not exceed 40 characters.")
+        else:
+            return player_name
+
+
 def main_menu():
-    """Function to print the main menu of the game"""
+    """Function to print the main menu of the game
+    and request the player to enter name before
+    playing the game"""
     print(Fore.YELLOW + """
     ____                           __   __                      
    / __ \\  ___    ____   ____ _   / /  / /_   __  __            
@@ -27,7 +44,11 @@ def main_menu():
     print("3. When saving, choose a goalpost to save the shot")
     print("    (LT, LB, C, RT, RB).")
     print("4. The game alternates between shooting and saving.")
-    print("5. Have fun!")
+    print("5. Have fun!\n")
+    print(Fore.BLUE + "Are you ready for the penalty challenge?")
+    return request_player_name()
+
 
 # Call the function to print the main menu
-main_menu()
+player_name = main_menu()
+print(Fore.GREEN + f"Welcome to the game, {player_name}!")
