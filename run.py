896c2f7a0e0bbc145b8b8bddaaeb99e4092ab504
Robert T <robert@tiffert.eu>
2024-04-13 22:19:16 +0000

    Add import random and toss coin function to decide who play first

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import random
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
@@ -16,9 +17,13 @@ def request_player_name():
     if the input contains other than A-Z alphabets or/and
     exceeds 40 characters"""
     while True:
-        player_name = input(Fore.YELLOW + "Enter your name to start the game: ")
+        player_name = input(
+                Fore.YELLOW + "\nEnter your name to start the game: ")
         if not player_name.isalpha():
-            print(Fore.RED + "Error: Name should only contain alphabets A-Z or a-z.")
+            print(
+                Fore.RED + "Error: Name should only"
+                "contain alphabets A-Z or a-z."
+                )
         elif len(player_name) > 40:
             print(Fore.RED + "Error: Name should not exceed 40 characters.")
         else:
@@ -58,6 +63,52 @@ def main_menu():
     return request_player_name()
 
 
-# Call the function to print the main menu
+def toss_coin(player_name):
+    """Function to handle the coin toss by asking the player
+    input to choose head or tail. The coin toss will simulate
+    after the player's input and the result output tells the
+    player who won the coin toss and who goes first in the game"""
+    clear_old_term()
+    print(Fore.GREEN + f"Welcome to the game, {player_name}!\n")
+    print("Let's toss a coin to decide who goes first in the penalty.")
+    print(Fore.BLUE + """
+    HEAD    |   TAIL
+            |   _____
+    |___|   |     |
+    |   |   |     |
+    """)
+    while True:
+        player_choice = input(
+                Fore.YELLOW + "Choose H for head or T for tail: ").upper()
+        if player_choice not in ['H', 'T']:
+            print(
+                Fore.RED + "Error: Please choose either H or T." + Fore.RESET
+                )
+        else:
+            break
+
+    # Simulate and display the coin toss result
+    coin_result = random.choice(['H', 'T'])
+    print(
+        Back.BLUE + f"\nThe coin toss result is: "
+        f"{'Head' if coin_result == 'H' else 'Tail'}\n"
+        + Back.RESET
+    )
+
+    # Decide who goes first based on the coin toss result
+    if player_choice == coin_result:
+        print(
+            Back.GREEN + "You won the coin toss!" + Back.RESET,
+            "You will go first.\n"
+            )
+    else:
+        print(
+            Back.RED + "You lost the coin toss." + Back.RESET,
+            "The opponent will go first.\n"
+            )
+
+
+# Call the various functions to play the game
 player_name = main_menu()
 print(Fore.GREEN + f"Welcome to the game, {player_name}!")
+toss_coin(player_name)
